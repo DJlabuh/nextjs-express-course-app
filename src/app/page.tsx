9189259dcb784ac8aef24a8a8d549635b28fc635
DJlabuh <djlabuh@gmail.com>
2024-06-17 21:58:04 +0300

    Reset NextJS commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
-import styles from "./page.module.css";
-import cource from "./course.json"
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <ul>
-      {cource.lessons.map((lesson)=>{
-        return <li key={lesson.name}>
-          <h1>{lesson.name}</h1>
-          <p>{lesson.title}</p>
-          <p>{lesson.points}</p>
-        </li>
-      })}
-      </ul>
+    <main>
     </main>
   );
 }
